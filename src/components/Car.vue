bdde7a0bc422d95620a22debea96400166386b7e
GabrielTrossero <gabrieltrosserogetr@gmail.com>
2021-10-28 18:09:26 -0300

    use of v-if, v-else-if and v-else

--- src/components/Car.vue
+++ src/components/Car.vue
@@ -2,60 +2,40 @@
   <h2>Coche</h2>
   <p>Marca: {{ brand }}</p>
   <p>Modelo: {{ model }}</p>
-  <p>Color:</p>
-  <!-- Uso como índice al propio color, pero si uno se repite, ya no sería único
+  <p>Potencias:</p>
+  <!-- Tengo que poner el v-for dentro de un template para que no me
+  muestre cada li (porque si lo evalua, ya lo muestra) -->
   <ul>
-    <li v-for="color in colors" :key="color">
-      {{ color }}
-    </li>
+    <template v-for="(power, index) in powers" :key="index">
+      <li v-if="power < 300">
+        {{ "Urbana: " + power }}
+      </li>
+      <li v-else-if="power >= 300 && power <= 350">
+        {{ "Hibrido: " + power }}
+      </li>
+      <li v-else>
+        {{ "Carrera: " + power }}
+      </li>
+    </template>
   </ul>
-  -->
-  <ul>
-    <!-- Genero un índice único -->
-    <li v-for="(color, index) in colors" :key="index">
-      {{ index + ": " + color }}
-    </li>
-  </ul>
-  <p>Precio: {{ price }}</p>
-  <p>Potencia: {{ power }}</p>
   <p>Datos: {{ messaje.title + " - " + messaje.description }}</p>
 </template>
 
 <script>
 export default {
-  /* VERSION 2 DE VUE:
-  data() {
-    return {
-      brand: "Audi",
-      model: "A4",
-      color: ["Azul", "Rojo", "Negro"],
-      price: 39000,
-      power: 250,
-      messaje: {
-        title: "Audi A4",
-        description: "Se vende",
-      },
-    };
-  },*/
-
-  // VERSION 3 DE VUE:
   setup() {
     const brand = "Audi";
     const model = "A4";
-    const colors = ["Azul", "Rojo", "Negro"];
-    const price = 39000;
-    const power = 250;
+    const powers = [220, 280, 350, 430, 550, 650];
     const messaje = {
       title: "Audi A4",
-      description: "Se vende",
+      description: "Se vende con 84 mil km",
     };
 
     return {
       brand,
       model,
-      colors,
-      price,
-      power,
+      powers,
       messaje,
     };
   },
